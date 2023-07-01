4e8f5c2b4761c2434ff1975ad86de224e1e4c16c
E1-XP <poczta.elektroniczna@hotmail.com>
2023-07-01 15:19:13 +0200

    finish footer RWD

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -41,13 +41,15 @@ const Footer = () => {
   };
 
   return (
-    <footer className="max-w-[1216px] px-min mx-auto">
-      <div className="flex justify-between border-b border-sandyAccent">
+    <footer className="max-w-[1216px] px-min mx-auto pt-[121px] md:pt-[90px] pb-9 md:pb-14">
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-between gap-[50px] md:gap-0 border-b border-sandyAccent pb-16 md:pb-[68px]">
         <h2 className="h4">{data.logoText}</h2>
         {data.columns.map((column) => (
           <section key={column.heading}>
-            <h3 className="h5">{column.heading}</h3>
-            <ul>
+            <h3 className="h5 mb-4 text-center md:text-start">
+              {column.heading}
+            </h3>
+            <ul className="text-center md:text-start">
               {column.links.map((link) => (
                 <li className="text" key={link.text}>
                   <Link to={link.to}>{link.text}</Link>
@@ -57,7 +59,7 @@ const Footer = () => {
           </section>
         ))}
         <section>
-          <ul className="flex gap-2">
+          <ul className="flex gap-2 md:-mt-2.5">
             {data.social.map((link) => (
               <li
                 className="h-12 w-12 bg-orange rounded-full text-white flex items-center justify-center"
@@ -69,9 +71,11 @@ const Footer = () => {
           </ul>
         </section>
       </div>
-      <div className="flex justify-between">
-        <p className="text">{data.copyrightText}</p>
-        <div>
+      <div className="flex flex-col md:flex-row items-center md:items-start justify-between mt-11 gap-8 md:gap-0">
+        <p className="text opacity-[64%] text-center md:text-start">
+          {data.copyrightText}
+        </p>
+        <div className="flex gap-[68px]">
           {data.links.map((link) => (
             <Link className="text" to={link.to} key={link.text}>
               {link.text}
