df177ae82ebe1c5132510280d9431c95d652821e
E1-XP <poczta.elektroniczna@hotmail.com>
2023-07-14 12:14:57 +0200

    finish section RWD

--- src/components/sections/FounderWords.tsx
+++ src/components/sections/FounderWords.tsx
@@ -12,7 +12,7 @@ const FounderWords = () => {
 
   return (
     <section className="bg-purple">
-      <div className="wrapper flex flex-col md:flex-row md:gap-8 xl:gap-[130px] pt-[90px] pb-[176px]">
+      <div className="wrapper flex flex-col md:flex-row gap-[41px] md:gap-16 lg:gap-[130px] pt-[58px] md:pt-[90px] pb-[88px] md:pb-[176px]">
         <div className="flex justify-center md:items-center md:w-1/2">
           <StaticImage
             src={"./../../images/founder.png"}
@@ -20,15 +20,19 @@ const FounderWords = () => {
             className="pointer-events-none"
           />
         </div>
-        <div className="md:w-[42%] relative">
+        <div className="md:w-[42%] relative flex flex-col mt-auto mb-auto">
           <StaticImage
             src={"./../../images/quotes.svg"}
             alt="quote marks"
-            className="pointer-events-none absolute top-0 right-[46px]"
+            className="pointer-events-none absolute top-2 right-0 md:right-[46px]"
           />
-          <h2 className="h5">{data.tagText}</h2>
-          <h3 className="h4 lg:h3 !text-white pt-[33px]">{data.heading}</h3>
-          <p className="text-big text-white pt-[44px]">{data.author}</p>
+          <h2 className="h5 z-10">{data.tagText}</h2>
+          <h3 className="h4 lg:h3 !text-white mt-[29px] md:mt-[33px] z-10">
+            {data.heading}
+          </h3>
+          <p className="text-big text-white pt-[23px] md:pt-[44px]">
+            {data.author}
+          </p>
           <p className="text-small text-white opacity-[0.64] pt-1.5">
             {data.occupation}
           </p>
